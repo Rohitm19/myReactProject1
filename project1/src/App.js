8a83537c160a771770a84fabf01e0ae9ac6b5c4a
Rohitm19 <mudaliar.extc.engg@gmail.com>
2021-12-08 00:10:57 +0530

    commit 2 - useState and its functionality.

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,9 +1,26 @@
-
+import React, { useState } from 'react';
+//import ReactDOM from 'react-dom';
+//import Tweet from './Tweet';
+import './App.css'
 
 function App() {
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+  };
+
   return (
-    <div >
-      <h1>Hello React</h1>
+    <div className="app">
+      {/* <Tweet name = "Rohit" message = "My react app"/>
+      <Tweet name = "Rohit" message = "My react app"/>
+      <Tweet name = "Rohit" message = "My react app"/>
+      <Tweet name = "Rohit" message = "My react app"/> */}
+      <h1 className= {isRed ? 'red' : ""}> Change my color! </h1> 
+      <button onclick={() => {increment();}}>Click here</button>
+      <h1>{count}</h1>
     </div>
   );
 }
--- project1/src/Tweet.js
+++ project1/src/Tweet.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import './App.css'
 
-function tweet() {
-    <div>
-        <h1>Name</h1>
-        <h2>Message</h2>
-        <h3>Number of likes</h3>
-    </div>
+function tweet({name, message}) {
+    return (
+        <div className = "tweet">
+            <h1>{name}</h1>
+            <h2>{message}</h2>
+            <h3>Number of likes</h3>
+        </div>
+    )
 }
 
 export default tweet;
