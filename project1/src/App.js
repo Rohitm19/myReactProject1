e31775565b3b867dcd2835bd30d709e2df8ab0da
Rohit_Mudaliar <Rohit.Mudaliar@ril.com>
2021-12-08 19:47:24 +0530

    commit 6 - useEffect example code.

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,21 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 //import ReactDOM from 'react-dom';
-import Tweet from './Tweet';
+// import Tweet from './Tweet';
 import './App.css'
 
 function App() {
-  const [users, setUsers] = useState([
-    { name: "Rohit", message: "Hello react" },
-    { name: "Jon Snow", message: "I am the true king!!!" },
-    { name: "CR7", message: "SIUUUU!!!!" }
-  ]);
+  const [count, setCount] = useState(0);
+
+  useEffect(()=> {
+    console.log("Effect has run")
+  }, [count]);
 
   return (
     <div className="app">
-      {users.map(user =>
-        <Tweet name={user.name} message={user.message} />
-      )}
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit" />
+      </form>
+      <h1 onClick={() => setCount(count + 1)}>{count}</h1>
     </div>
   );
 }
 export default App;
+
+// Tip:
+// Observe console in browser and useEffect output once count is clicked.
+// if the second argument that is an array inside the useEffect is empty then the useEffectwill run once.
+// and if we put count inside the array then whenever the count is clicked the useEfffect will be executed.  
