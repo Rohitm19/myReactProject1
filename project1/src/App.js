ee8bb151a8563659727de9ca3ee6e0fdd5c4c690
Rohit_Mudaliar <Rohit.Mudaliar@ril.com>
2021-12-09 10:21:24 +0530

    commit 7 - my recipe app using useEffect, useStates, props and css module.

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,28 +1,57 @@
 import React, { useEffect, useState } from 'react';
 //import ReactDOM from 'react-dom';
 // import Tweet from './Tweet';
+import Recipe from './Recipe'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [querry, setQuerry] = useState('chicken');
 
-  useEffect(()=> {
-    console.log("Effect has run")
-  }, [count]);
+  const APP_ID = "ce39fa51";
+  const APP_KEY = "7708a1a501f3efc483405be929203c0f";
+  const APP_URL = `https://api.edamam.com/search?q=${querry}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(() => {
+    getRecipes();
+  }, [querry]);
+
+  const getRecipes = async () => {
+    const response = await fetch(APP_URL);
+    const data = await response.json();
+    setRecipes(data.hits);
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value); //e is the event, which in this case is change, target is the element that triggered the event, which in this case is the input, and value is the value of the input element
+  }
+
+  const getSearch = e => {
+    e.preventDefault() ///to prevent page refresh
+    setQuerry(search);
+    setSearch('');
+  }
 
   return (
     <div className="app">
-      <form className="search-form">
-        <input className="search-bar" type="text" />
-        <button className="search-button" type="submit" />
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit"> search </button>
       </form>
-      <h1 onClick={() => setCount(count + 1)}>{count}</h1>
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
 }
 export default App;
 
-// Tip:
-// Observe console in browser and useEffect output once count is clicked.
-// if the second argument that is an array inside the useEffect is empty then the useEffectwill run once.
-// and if we put count inside the array then whenever the count is clicked the useEfffect will be executed.  
